4229a8d9d692dad594e9b978d7562625588336cb
liuyang <yang1.liu@chinatalnetgroup.com>
2020-12-10 16:30:03 +0800

    项目启动调试

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ module.exports = {
     // 配置反向代理，解决跨域请求
     proxyTable: {
       '/api': {
-        target: 'http://127.0.0.1:8080',
+        target: 'http://127.0.0.1:8089',
         changeOrigin: true,
         pathRewrite: {
           '^/': ''
